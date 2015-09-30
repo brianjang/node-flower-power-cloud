6b4e1a6ca2c6ae4dd1938a52e73d870e5e16b2c3
bsautron <sautron.brunojj@gmail.com>
2015-09-30 15:46:33 +0200

    New api version

--- flower-power-cloud.js
+++ flower-power-cloud.js
@@ -3,20 +3,19 @@
 
 
 var https       = require('https')
-  , events      = require('events')
-  , querystring = require('querystring')
-  , url         = require('url')
-  , util        = require('util')
-  , request     = require('request')
-  ;
+, events      = require('events')
+, querystring = require('querystring')
+, url         = require('url')
+, util        = require('util')
+;
 
 
 var DEFAULT_LOGGER = { error   : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
-                     , warning : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
-                     , notice  : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
-                     , info    : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
-                     , debug   : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
-                     };
+, warning : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
+, notice  : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
+, info    : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
+, debug   : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
+};
 
 
 var CloudAPI = function(options) {
@@ -46,30 +45,30 @@ CloudAPI.prototype.login = function(username, passphrase, callback) {
   if (typeof callback !== 'function') throw new Error('callback is mandatory for login');
 
   json = { username      : username
-         , client_secret : self.options.clientSecret
-         , password      : passphrase
-         , client_id     : self.options.clientID
-         , grant_type    : 'password'
-         };
-  self.invoke('POST', '/user/v1/authenticate', json, function(err, code, results) {
+    , client_secret : self.options.clientSecret
+    , password      : passphrase
+    , client_id     : self.options.clientID
+    , grant_type    : 'password'
+  };
+  self.invoke('POST', '/user/v2/authenticate', json, function(err, code, results) {
     if (!!err) callback(err);
 
     if (code !== 200) return callback(new Error('invalid credentials: code=' + code + ' results=' + JSON.stringify(results)));
 
     self.oauth = results;
 
-/*
+    /*
     if (!!self.timer) {
-      clearTimeout(self.timer);
-      delete(self.timer);
-    }
-    if (!!results.expires_in) self.timer = setTimeout(function() { self._refresh(self); }, (results.expires_in - 120) * 1000);
- */
+    clearTimeout(self.timer);
+    delete(self.timer);
+  }
+  if (!!results.expires_in) self.timer = setTimeout(function() { self._refresh(self); }, (results.expires_in - 120) * 1000);
+  */
 
-    callback(null);
-  });
+  callback(null);
+});
 
-  return self;
+return self;
 };
 
 CloudAPI.prototype._refresh = function(self, callback) {
@@ -86,11 +85,11 @@ CloudAPI.prototype._refresh = function(self, callback) {
   }
 
   json = { client_id     : self.options.clientID
-         , client_secret : self.options.clientSecret
-         , refresh_token : self.oauth.refresh_token
-         , grant_type    : 'refresh_token'
-         };
-  self.invoke('POST', '/user/v1/authenticate', json, function(err, code, results) {
+    , client_secret : self.options.clientSecret
+    , refresh_token : self.oauth.refresh_token
+    , grant_type    : 'refresh_token'
+  };
+  self.invoke('POST', '/user/v2/authenticate', json, function(err, code, results) {
     if (!!err) callback(err);
 
     if (code !== 200) return callback(new Error('invalid credentials: code=' + code + 'results=' + JSON.stringify(results)));
@@ -108,35 +107,45 @@ CloudAPI.prototype._refresh = function(self, callback) {
 
 CloudAPI.prototype.getGarden = function(callback) {
   var self = this;
-  var tabSensorSerial  = [];
-  var tabHistoryIndex = [];
 
-  return self.invoke('GET', '/sensor_data/v3/sync?include_s3_urls=1', function(err, code, results) {
-    var count, i, location, locations, sensor, sensors, user_config_version;
+  return self.invoke('GET', '/sensor_data/v4/garden_locations_status', function(err, code, results) {
+    var count, i, location, locations, sensor, sensors, last_sample_utc, first_sample_utc;
 
     if (!!err) return callback(err);
 
     var f = function(id) {
       return function(err, results) {
-          if (!!err) self.logger.error('invoke', { event: 'sync', diagnostic: err.message });
+        if (!!err) self.logger.error('invoke', { event: 'sync', diagnostic: err.message });
         else locations[id].samples = results.samples;
 
-        if (--count === 0) callback(null, locations, sensors, tabSensorSerial, tabHistoryIndex, user_config_version);
+        if (--count === 0) callback(null, locations, sensors);
       };
     };
 
     count = 0;
-    user_config_version = results.user_config_version;
+
 
     sensors = {};
     for (i = 0; i < results.sensors.length; i++) {
       sensor = results.sensors[i];
-      tabSensorSerial.push(sensor.sensor_serial);
       sensors[sensor.sensor_serial] = sensor;
     }
 
+    locations = {};
+    for (i = 0; i < results.locations.length; i++) {
+      location = results.locations[i];
+      locations[location.location_identifier] = location;
+
+      count++;
+      last_sample_utc = new Date(location.last_sample_utc);
+      first_sample_utc = new Date(last_sample_utc);
+      first_sample_utc.setUTCDate(first_sample_utc.getUTCDate() - 7);
+      self.roundtrip('GET', '/sensor_data/v3/sample/location/' + location.location_identifier
+      + '?from_datetime_utc=' + first_sample_utc.toISOString() + '&to_datetime_utc=' + last_sample_utc.toISOString(), f(location.location_identifier));
+    }
+
     count++;
-    self.roundtrip('GET', '/sensor_data/v4/garden_locations_status', function(err, results) {
+    self.roundtrip('GET', '/sensor_data/v1/garden_locations_status', function(err, results) {
       var i, id;
 
       if (!!err) self.logger.error('invoke', { event: 'garden_locations_status', diagnostic: err.message });
@@ -145,77 +154,36 @@ CloudAPI.prototype.getGarden = function(callback) {
           location = results.locations[i];
           id = location.location_identifier;
           delete(location.location_identifier);
-          
-        for (i = 0; i < results.sensors.length; i++) {
-          sensor = results.sensors[i];
-          tabHistoryIndex.push(sensor.current_history_index);
-    }
 
-        }          
+          locations[id].status = location;
+        }
       }
 
-      if (--count === 0) callback(null, sensors, tabSensorSerial, tabHistoryIndex, user_config_version);
+      if (--count === 0) callback(null, locations, sensors);
     });
   });
 };
 
-CloudAPI.prototype.uploadGarden = function (sensor_serial, user_config_version, sUpTime, history, currentID, sessionPeriod, sessionStartIndex, callback) {
+CloudAPI.prototype.getUserConfig = function(callback) {
+  var self = this;
 
-var self = this;
-var json1 =  {
-            client_datetime_utc : new Date(),
-            user_config_version : user_config_version,
-            tmz_offset : (new Date()).getTimezoneOffset(),
-            
-            session_histories : [{
-               sensor_serial : sensor_serial,
-               session_id : currentID,
-               sample_measure_period : sessionPeriod,
-               sensor_startup_timestamp_utc : sUpTime,
-               session_start_index : sessionStartIndex
-            }],
-            
-            uploads : [{
-               sensor_serial : sensor_serial,
-               upload_timestamp_utc : new Date(),
-               buffer_base64 : history,
-               app_version : "",
-               sensor_fw_version : "",
-               sensor_hw_identifier : "",
-            }]
-           };
-           
-      request({
-         proxy: undefined,
-         method: 'PUT',
-         uri: 'https://apiflowerpower.parrot.com/sensor_data/v5/sample',
-         headers: {Authorization : "Bearer " + self.oauth.access_token},
-         body: (json1 !== null)? JSON.stringify(json1) : undefined
-      }, function (error, response, body) {
-         if (error !== null) {
-            callback(error, null);
-         } else if (response.statusCode !== 200) {
-            var htmlError = self.manageHtmlErrors(response);
-            if (typeof (callback) === 'function') {
-               callback(htmlError, body);
-            }
-         } else {
-            body = JSON.parse(body);
-            if((body.hasOwnProperty("errors") && (body.errors.length > 0))){
-               callback(body.errors[0], null);  
-            }
-            else {
-               if (self.formatter instanceof Function){
-                  body = self.formatter(body);
-               }
-               if (callback instanceof Function) {
-                  callback(null, body);
-               }
-            }
-         }
-      });
-};
+  return self.invoke('GET', '/user/v4/profile', function(err, code, results) {
+    callback(err, results);
+  });
+}
 
+CloudAPI.prototype.sendHistory = function(param, callback) {
+  var self = this;
+
+  return self.invoke('PUT', '/sensor_data/v5/sample', JSON.stringify(param), function(err, code, results) {
+    if (err || code > 210) {
+      callback(err, body);
+    }
+    else {
+      callback(err, param.uploads[0].buffer_base64);
+    }
+  });
+}
 
 CloudAPI.prototype.roundtrip = function(method, path, json, callback) {
   var self = this;
@@ -263,22 +231,28 @@ CloudAPI.prototype.invoke = function(method, path, json, callback) {
     options.headers.Authorization = 'Bearer ' + self.oauth.access_token;
   }
   if (!!json) {
-    options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-    json = querystring.stringify(json);
+    if (method == 'PUT') {
+      options.headers['Content-Type'] = 'application/json';
+    }
+    else {
+      options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+      json = querystring.stringify(json);
+    }
     options.headers['Content-Length'] = Buffer.byteLength(json);
   }
 
-  https.request(options, function(response) {
+  var req = https.request(options, function(response) {
     var body = '';
 
     response.on('data', function(chunk) {
       body += chunk.toString();
-    }).on('end', function() {
+    });
+    response.on('end', function() {
       var expected = { GET    : [ 200 ]
-                     , PUT    : [ 200 ]
-                     , POST   : [ 200, 201, 202 ]
-                     , DELETE : [ 200 ]
-                     }[method];
+        , PUT    : [ 200 ]
+        , POST   : [ 200, 201, 202 ]
+        , DELETE : [ 200 ]
+      }[method];
 
       var results = {};
 
@@ -288,17 +262,22 @@ CloudAPI.prototype.invoke = function(method, path, json, callback) {
       }
 
       if (expected.indexOf(response.statusCode) === -1) {
-         self.logger.error(path, { event: 'https', code: response.statusCode, body: body });
-         return callback(new Error('HTTP response ' + response.statusCode), response.statusCode, results);
+        self.logger.error(path, { event: 'https', code: response.statusCode, body: body });
+        return callback(new Error('HTTP response ' + response.statusCode), response.statusCode, results);
       }
-
+      response.removeAllListeners('close');
       callback(null, response.statusCode, results);
-    }).on('close', function() {
+    });
+    response.on('close', function() {
       callback(new Error('premature end-of-file'));
-    }).setEncoding('utf8');
-  }).on('error', function(err) {
+    });
+    response.setEncoding('utf8');
+  });
+
+  req.on('error', function(err) {
     callback(err);
-  }).end(json);
+  });
+  req.end(json);
 
   return self;
 };
--- test.js
+++ test.js
@@ -1,21 +1,26 @@
-var CloudAPI = require('./flower-power-cloud');
+var CloudAPI = require('./flower-power-cloud').CloudAPI;
 
-var clientID     = '...'
-  , clientSecret = '...'
-  , userName     = '...'
-  , passPhrase   = '...'
-  , api
-  ;
+var clientID     = 'parrottest.fpwebservice@gmail.com'
+, clientSecret = 'cvSjfnONllkHLymF2gEUL73PPXJiMMcVCd1VtZaIXHSGyhaT'
+, userName     = 'parrottest.fpwebservice@gmail.com'
+, passPhrase   = 'Parrot2015FP'
+, api
+;
 
-api = new CloudAPI.CloudAPI({ clientID: clientID, clientSecret: clientSecret }).login(userName, passPhrase, function(err) {
-  if (!!err) return console.log('login error: ' + err.message);
-
-  api.getGarden(function(err, plants, sensors) {
-    if (!!err) return console.log('getGarden: ' + err.message);
-
-    console.log('plants:'); console.log(plants);
-    console.log('sensors:'); console.log(sensors);
-  });
-}).on('error', function(err) {
-  console.log('background error: ' + err.message);
+api = new CloudAPI({
+	clientID: clientID,
+	clientSecret: clientSecret
 });
+
+api.login(userName, passPhrase, function(err) {
+	if (!!err) return console.log('login error: ' + err.message);
+
+	api.getGarden(function(err, plants, sensors) {
+		if (!!err) return console.log('getGarden: ' + err.message);
+
+		console.log('plants:'); console.log(plants);
+		console.log('sensors:'); console.log(sensors);
+	});
+});
+
+api.on('error', function(err) { console.log('background error: ' + err.message); });
